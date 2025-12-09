5cea65a414409c761733a657557586779c2aeacc
timurg01 <81186291+timurg01@users.noreply.github.com>
2025-12-09 14:58:22 -0800

    Added fixed navigation and completed reveal elements on scroll.

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ const testimonialBlock = document.querySelectorAll(".testimonial-block");
 const allSections = document.querySelectorAll(".section");
 
 const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section-hidden");
-  observer.unobserve(entry.target);
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove("section-hidden");
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -22,3 +23,23 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section-hidden");
 });
+
+//STICKY NAVIGATION
+const header = document.querySelector("#hero");
+const nav = document.querySelector(".nav");
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "100px",
+});
+
+headerObserver.observe(header);
