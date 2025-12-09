328644969268219123d2842153bd11d867caff93
timurg01 <81186291+timurg01@users.noreply.github.com>
2025-12-08 16:04:14 -0800

    added smooth scrolling and reveal elements on scroll

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+
+//TESTIMONIAL CAROUSEL
+const testimonialParentEl = document.querySelector(".carousel");
+const testimonialBlock = document.querySelectorAll(".testimonial-block");
+
+//REVEAL ELEMENTS ON SCROLL
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
