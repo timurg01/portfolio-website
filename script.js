8012260a48af79dcded603a43265f08dc450f5a5
timurg01 <81186291+timurg01@users.noreply.github.com>
2025-12-10 14:50:53 -0800

    Completed carousel windows, to complete mobile responsiveness

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ const nav = document.querySelector(".nav");
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
@@ -43,3 +42,83 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+//CAROUSEL
+const slides = document.querySelectorAll(".testimonial-block");
+const carousel = document.querySelector(".carousel");
+const btnLeft = document.querySelector(".carousel--btn-left");
+const btnRight = document.querySelector(".carousel--btn-right");
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+//change slide function
+const goToSlide = function (slide = 0) {
+  slides.forEach((s, i) => {
+    s.style.transform = `translateX(${(i - currentSlide) * 100}%)`;
+  });
+};
+
+goToSlide();
+//go to next slide event - ARROWS
+
+//go to next slide event - DOTS
+const dotContainer = document.querySelector(".dots");
+
+const createDots = function () {
+  slides.forEach(function (s, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots-dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+createDots();
+
+const activeDot = function (slide) {
+  document
+    .querySelectorAll(".dots-dot")
+    .forEach((dot) => dot.classList.remove("dot-active"));
+
+  document
+    .querySelector(`.dots-dot[data-slide="${slide}"]`)
+    .classList.add("dot-active");
+};
+
+//initial dot activation
+activeDot(0);
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots-dot")) {
+    currentSlide = Number(e.target.dataset.slide);
+    goToSlide(currentSlide);
+    activeDot(currentSlide);
+  }
+});
+
+const nextSlide = function () {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  goToSlide(currentSlide);
+  activeDot(currentSlide);
+};
+
+const prevSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+
+  goToSlide(currentSlide);
+  activeDot(currentSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+
+btnLeft.addEventListener("click", prevSlide);
